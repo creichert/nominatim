336e9563d5e3aac0bcc22f3fe687dfb2f4dcc614
Christopher Reichert <creichert07@gmail.com>
2015-07-09 20:46:14 -0500

    Implement reasonable defaults and optional command-line flags.

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Maybe
 import qualified Data.Text                  as T
 import           Options.Applicative
 
@@ -12,42 +13,47 @@ import           Web.Nominatim
 
 
 data NominatimCli = NominatimCli
-  { userAgent :: !String
-  , query     :: !String
-  , email     :: !String
-  , limit     :: !Int
+  { query     :: !String
+  , userAgent :: !(Maybe String)
+  , email     :: !(Maybe String)
+  , limit     :: !(Maybe Int)
   } deriving Show
 
 
 nominatimCli :: Parser NominatimCli
 nominatimCli = NominatimCli
   <$> strOption
-      ( long "user-agent"
-      <> metavar "USER-AGENT"
-      <> help "User-Agent header value" )
-  <*> strOption
       ( long "query"
       <> metavar "ADDRESS"
       <> help "Address query" )
-  <*> strOption
-      ( long "email"
-      <> metavar "EMAIL"
-      <> help "Email address to send with request" )
-  <*> option auto
-      ( long "limit"
-      <> metavar "LIMIT"
-      <> help "Limit the number of results" )
+  <*> optional
+      ( strOption
+        ( long "user-agent"
+        <> metavar "USER-AGENT"
+        <> help "User-Agent header value" ) )
+  <*> optional
+      (strOption
+       ( long "email"
+       <> metavar "EMAIL"
+       <> help "Email address to send with request" ) )
+  <*> optional
+      ( option auto
+        ( long "limit"
+        <> metavar "LIMIT"
+        <> help "Limit the number of results" ) )
 
 
 nominatimRun :: NominatimCli -> IO ()
 nominatimRun NominatimCli{..} = do
-  r <- nominatim [("User-Agent", BL.pack userAgent)]
-                 [ Format Json
-                 , Query (T.pack query)
-                 , Email (T.pack email)
-                 , Limit limit
-                 , AddressDetails True
-                 ]
+
+  let ua = fromMaybe "Nominatim.hs" userAgent
+      opts = [ Format Json
+             , Query (T.pack query)
+             , AddressDetails True
+             , Limit (fromMaybe 10 limit)
+             ] ++ (maybe [] (\e -> [ Email (T.pack e) ]) email)
+
+  r <- nominatim [("User-Agent", BL.pack ua)] opts
 
   putStrLn $ BL.unpack r
   -- TODO support more stdout output formats
